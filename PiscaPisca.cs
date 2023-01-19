bfcd701e0af88c8f083b33e383434f350e8938be
Evandru <evandro_remolli.t@outlook.com>
2023-01-19 13:55:05 -0300

    First commit of ChristmasStringLights

---
+++ PiscaPisca.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasStringLights
+{
+    public static class PiscaPisca
+    {
+        public static void Run()
+        {
+            Console.Write("Escreva uma frase / palavra: ");
+            string palavra = Console.ReadLine();
+            Console.Clear();
+            while(true)
+            {
+                for (int i = 0; i < palavra.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(palavra[i]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(palavra[i]);
+                    }
+                }
+                Thread.Sleep(1);
+                Console.Clear();
+                for (int i = 0; i < palavra.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(palavra[i]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(palavra[i]);
+                    }
+                }
+                Thread.Sleep(1);
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿using ChristmasStringLights;
+
+PiscaPisca.Run();
